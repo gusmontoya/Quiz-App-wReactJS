8610cd96fcd0ae67de0c492f762f9fcc771b574c
gusmontoya <gus.montoya@gmail.com>
2021-10-30 19:31:20 -0700

    testing basic styling with tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ const API_URL = 'https://opentdb.com/api.php?amount=10';
 
 function App() {
   return (
-    <h1>Hello Gusters</h1>
+    <div class="container">
+      <div class='bg-white text-green-800 p-10 rounded-lg shadow-md'>
+        <h2 className='text-2xl'>
+        Welcome to the Tivia app
+        </h2>
+      </div>
+      <div>Question amount picker</div>
+      <div>Start button</div>
+    </div>
   );
 }
 
